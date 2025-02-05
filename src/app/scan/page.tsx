ada27a760164ef2b70834224312a13c6bce8553d
wojd0 <wojtekd2000@gmail.com>
2025-02-06 00:34:49 +0100

    scan and links

---
+++ src/app/scan/page.tsx
@@ -0,0 +1,39 @@
+export default function Scan() {
+  return (
+    <div className="h-screen bg-base-400">
+      Scan
+      <form>
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
+}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 export default function Hero() {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen bg-base-400">
             <div className="hero-content text-center">
                 <div className="max-w-md">
                     <h1 className="text-5xl font-bold">Paragonizator, paragonizer, paragoinator...</h1>
                     <p className="py-6">Nie mam pomysłu na nazwę ☹️</p>
-                    <button className="btn btn-primary">Rozpocznij</button>
+                    <Link href="/scan">
+                        <button className="btn btn-primary">Rozpocznij</button>
+                    </Link>
                 </div>
             </div>
         </div>
