2e83c54fb7b714e21eddf5746bfb3d01fd3ed2d5
wojd0 <wojciechduda03@gmail.com>
2025-10-13 00:22:02 +0200

    refactor ReceiptList and TotalFlagPanel components to calculate and manage flags more efficiently; add onRemove functionality for flag removal

--- src/components/scan-results/receipt-list/total-flag-panel/TotalFlagPanel.tsx
+++ src/components/scan-results/receipt-list/total-flag-panel/TotalFlagPanel.tsx
@@ -1,45 +1,26 @@
-import { PlusIcon } from "@heroicons/react/16/solid";
-import { useMemo } from "react";
-import { ReceiptItem } from "../receipt-list.types";
-import TotalFlag from "./TotalFlag";
+import { PlusIcon } from '@heroicons/react/16/solid';
+import TotalFlag from './TotalFlag';
 
 export default function TotalFlagPanel({
-	items = [],
+   flags = {},
+   onFlagChange,
 }: {
-	items?: ReceiptItem[];
+   flags?: Record<string, number>;
+   onFlagChange: (flags: Record<string, number>) => void;
 }) {
-	const calculatedFlags = useMemo<Record<string, number>>(() => {
-		const result = items.reduce(
-			(acc, item) => {
-				if (item.selectedFlags.length === 0) {
-					acc.unflaggedTotal += item.totalPrice;
-				} else {
-					item.selectedFlags.forEach((flag) => {
-						acc.calculatedSelectedFlags[flag] =
-							(acc.calculatedSelectedFlags[flag] || 0) +
-							item.totalPrice / item.selectedFlags.length;
-					});
-				}
-				return acc;
-			},
-			{
-				calculatedSelectedFlags: {} as Record<string, number>,
-				unflaggedTotal: 0,
-			},
-		);
 
-		return {
-			transparent: result.unflaggedTotal,
-			...result.calculatedSelectedFlags,
-		};
-	}, [items]);
+   function handleRemoveFlag(flagId: string) {
+      const newFlags = { ...flags };
+      delete newFlags[flagId];
+      onFlagChange(newFlags);
+   }
 
-	return (
-		<div className="card bg-base-200 w-full min-h-16">
-			<div className="card-content p-3 flex flex-wrap">
-				{Object.entries(calculatedFlags).map(([flagId, total]) => (
-					<TotalFlag key={flagId} flagId={flagId} total={total} />
-				))}
+   return (
+      <div className='card bg-base-200 w-full min-h-16'>
+         <div className='card-content p-3 flex flex-wrap'>
+            {Object.entries(flags).map(([flagId, total]) => (
+               <TotalFlag key={flagId} flagId={flagId} total={total} onRemove={() => handleRemoveFlag(flagId)} />
+            ))}
 
 				<button className="btn bg-base-300 whitespace-nowrap mx-auto md:mx-0">
 					<PlusIcon className="w-6" />
